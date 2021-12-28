1e8146ad64a8ef86f759467b867cc47609cdedd2
zsline <sergei.dereka@gmail.com>
2021-12-28 17:24:46 +0200

    header add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="../images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="../images/next.svg" alt=""></button>',
+    vertical: true,
+    }
+  );
 });
