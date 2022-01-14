11f09a8c205de2e6146dd15c8b11e31170ba10e7
zsline <sergei.dereka@gmail.com>
2022-01-14 13:30:57 +0200

    js puth

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
   $('.header-slider').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="../images/prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="../images/next.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="./images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="./images/next.svg" alt=""></button>',
     vertical: true,
     responsive: [
       {
@@ -18,8 +18,8 @@ $(function(){
     });
 
   $('.product__name').slick({
-    prevArrow: '<button type="button" class="name-prev name-btn"><img src="../images/name-prev.svg" alt=""></button>',
-    nextArrow: '<button type="button" class="name-next name-btn"><img src="../images/name-next.svg" alt=""></button>',
+    prevArrow: '<button type="button" class="name-prev name-btn"><img src="./images/name-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="name-next name-btn"><img src="./images/name-next.svg" alt=""></button>',
     slidesToShow: 4,
     slidesToScroll: 1,
     centerMode: true,
